a261d9bad0c491e28cfe7b0d7ddb4f70c36dfa8a
CHOILOCAL <giants1217@naver.com>
2021-06-14 20:20:02 +0900

    header/footer/modal

--- src/pages/header.js
+++ src/pages/header.js
@@ -127,46 +127,6 @@ const Header = () => {
         </Modal.Footer>
       </Modal>
     </>
-    // <div>
-    //     <ul>
-    //         <li>
-    //             <NavLink to="/" activeStyle={{ background: 'red', color: 'black' }}>홈</NavLink>
-    //         </li>
-    //         <li>
-    //             <Link to="/aboutDamdeeng">담딩투어 소개</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/product">여행상품</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/makeproduct">나만의여행상품만들기</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/review">리뷰</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/qna">문의</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/withme">동행구하기</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/sns">담딩 SNS</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/history">예제</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/Bootstrap">BootstrapSample</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/d3">D3Sample</Link>
-    //         </li>
-    //         <li>
-    //             <Link to="/icon">IconMeterialSample</Link>
-    //         </li>
-    //     </ul>
-    // </div>
   );
 };
 
